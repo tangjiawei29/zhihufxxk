8a70efb9ead5869d135de6feaf114aee574db0a2
SHI_FU <tangjiawei18@gmail.com>
2020-06-03 14:33:22 +0800

    Update user.js

--- user.js
+++ user.js
@@ -1,14 +1,13 @@
 // ==UserScript==
 // @name         知乎Fxxk
 // @namespace    http://tampermonkey.net/
-// @version      1.4
-// @description  移除知乎故事档案局的回答
+// @version      1.5
+// @description  移除知乎故事档案局的回答,推荐页广告
 // @author       SHIFU
 // @grant        none
 // @match        www.zhihu.com/*
 
 // ==/UserScript==
-
 (function() {
     'use strict';
     var obj = document.getElementsByClassName("ContentItem AnswerItem");
@@ -17,12 +16,17 @@
         if(trashNode.indexOf("\"authorName\":\"故事档案局\"")>=0
         || trashNode.indexOf("\"authorName\":\"盐选推荐\"")>=0){
             obj[i].style.display = "none";
+
         }
     }
     setInterval(function(){
-    var adv = document.getElementsByClassName("Pc-feedAd-container ");
-    for(var j = 0;j < adv.length; j++){
-        adv[j].style.display = "none";
+        var adv = document.getElementsByClassName("Pc-feedAd-container ");
+        for(var j = 0;j < adv.length; j++){
+            adv[j].style.display = "none";
+        }
+        var linkCard = document.getElementsByClassName("RichText-MCNLinkCardContainer");
+        for(var m = 0;m < linkCard.length; m++){
+            linkCard[m].style.display = "none";
         }
     },1500);
 })();
