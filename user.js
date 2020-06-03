51bef9f69e68d8ed116e66bc8b27b11cc47a67c5
SHI_FU <tangjiawei18@gmail.com>
2020-06-03 09:43:23 +0800

    Update user.js

--- user.js
+++ user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         知乎Fxxk
 // @namespace    http://tampermonkey.net/
-// @version      1.3
+// @version      1.4
 // @description  移除知乎故事档案局的回答
 // @author       SHIFU
 // @grant        none
@@ -14,14 +14,15 @@
     var obj = document.getElementsByClassName("ContentItem AnswerItem");
     for(var i = 0;i < obj.length; i++){
         var trashNode=obj[i].attributes["data-zop"].value;
-        if(trashNode.indexOf("\"authorName\":\"故事档案局\"")>=0 
+        if(trashNode.indexOf("\"authorName\":\"故事档案局\"")>=0
         || trashNode.indexOf("\"authorName\":\"盐选推荐\"")>=0){
             obj[i].style.display = "none";
         }
     }
-    
-    var adv = document.getElementsByClassName("Pc-feedAd-container");
-     for(var j = 0;j < adv.length; j++){
-         adv[j].style.display = "none";
-     }
+    setInterval(function(){
+    var adv = document.getElementsByClassName("Pc-feedAd-container ");
+    for(var j = 0;j < adv.length; j++){
+        adv[j].style.display = "none";
+        }
+    },1500);
 })();
