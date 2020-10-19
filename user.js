a9d95c26c3cf4b5b4e2ca7d0069bf2e08572d15b
SHI_FU <tangjiawei18@gmail.com>
2020-10-19 16:51:35 +0800

    Update user.js
    屏蔽 真实故事计划,回答页中广告

--- user.js
+++ user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         知乎Fxxk
-// @version      1.9
+// @version      2.0
 // @namespace    undefined
-// @description  移除知乎官方的回答,推荐页广告
+// @description  移除知乎官方的回答,推荐页广告,回答页中广告
 // @author       SHIFU
 // @grant        none
 // @match        *://www.zhihu.com/*
@@ -12,9 +12,11 @@
     'use strict';
     const answerIgnoreList = new Array("故事档案局", "盐选推荐", "真实职业故事","盐选健康必修课","盐选科普","知乎盐选会员","知乎盐选会员",
                                        "盐选科技前沿","盐选文学甄选","盐选成长计划","盐选会员精品","盐选点金",
-                                       "盐选生活馆","盐选职场","盐选奇妙物语","盐选博物馆","亲历者说","盐选心理","知乎盐选创作者");
+                                       "盐选生活馆","盐选职场","盐选奇妙物语","盐选博物馆","亲历者说","盐选心理","知乎盐选创作者","真实故事计划");
+
     const elementIgnoreList = new Array("Pc-feedAd-container ", "RichText-MCNLinkCardContainer",
-                                        "Pc-card Card","Card-section RelatedCommodities-list","Card-section RelatedCommodities-list");
+                                        "Pc-card Card","Card-section RelatedCommodities-list","Card-section RelatedCommodities-list","ecommerce-ad-box",
+                                       "RichText-ADLinkCardContainer");
     setInterval(function(){
         var obj = document.getElementsByClassName("ContentItem AnswerItem");
         for(var i = 0;i < obj.length; i++){
